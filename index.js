bae325b7ce9ebc863e1b9fc8e3ef6ffb7aeb2d38
Jawaharlal_nehru123 <jawaharlal@wolkensoftware.com>
2023-03-25 08:47:41 +0530

    added two files together

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("angular.json");
+const router = jsonServer.router(["course.json", "angular.json"]);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
